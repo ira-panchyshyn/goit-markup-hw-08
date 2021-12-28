4d1ee7bccdc8eb6b6f2b91bbc2987d63e2c9a5e1
Ira Panchyshyn <ira.panchyshyn@gmail.com>
2021-12-29 00:00:49 +0200

    added mob style to modal

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,7 +1,7 @@
 (() => {
-  const mobileMenu = document.querySelector('.js-menu-container');
-  const openMenuBtn = document.querySelector('.js-open-menu');
-  const closeMenuBtn = document.querySelector('.js-close-menu');
+  const mobileMenu = document.querySelector('.mob-menu');
+  const openMenuBtn = document.querySelector('.header-button__open');
+  const closeMenuBtn = document.querySelector('.mob-menu__close-btn');
 
   const toggleMenu = () => {
     const isMenuOpen =
